bf4b725af8e29939a4b0f9813e808c087b4b3611
rahul-vishwakarma-05 <116077468+rahul-vishwakarma-05@users.noreply.github.com>
2022-11-26 15:44:33 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,3 +1,4 @@
+
 let calcy = () =>{
     let web = document.querySelector("#webs").value;
     let maths = document.querySelector("#maths").value;
@@ -6,35 +7,55 @@ let calcy = () =>{
     let total;
     let percent;
     let grade = " ";
+      
 
     total = parseInt(web) + parseInt (maths) + parseInt(comp) + parseInt(phy);
-    alert(total);
+     
+     let x;
+     x = total;
+     
+        if( (isNaN(x)) == true){
+        alert("wrong");
+        return;
+     }
+     
 
     percent = (total/400) * 100;
-    alert(percent + "%");
     
     if(percent <= 100 && percent >= 80)
     {
         grade ="A";  
-        alert(grade);    
     }
     else if(percent <= 79 && percent >= 60)
     {
         grade ="B";  
-        alert(grade);
     }
     else if(percent <= 59 && percent >= 40)
     {
         grade ="c";  
-        alert(grade);
     }
     else
     {
-        grade ="f";  
-        alert(grade);
+        grade ="f";
     }
-    document.getElementById("notes").innerText = `out of 400 your total marks ${total} and your grades is ${grade}`; 
+    
+    document.getElementById("notes").innerHTML = ` <h1> out of 400 your total MARKS :- ${total} <br> and your PERCENT :- is ${percent} <br> your GRADES :- ${grade} </h1>`;
+    
+    
+    let card = document.querySelector(".show");
+    let ptag = document.querySelector("#ptag");     
+    let cardtwo = document.querySelector(" .flip-card-inner "); 
+    card.addEventListener("click", () => {
+    cardtwo.style.transform = "translateX(-100%) rotateY(-180deg)";
+   });
+   
+  let cardthree = document.querySelector(".flip-card-back");
+     cardthree.addEventListener("click", () => {
+        cardtwo.style.transform = "translateX(0) rotateY(0deg)";   
+  });
+
+
 
 }
-
-    // dynamic chnge grdaes and marks by user
+       
+ 
